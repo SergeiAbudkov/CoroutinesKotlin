e48ea28dc41686ba2b5be11feee054f9799fc9dc
Abudkov Sergei <abudkovkotlin@gmail.com>
2024-06-28 17:36:37 +0300

    Refactoring Dialogs: Done

--- app/src/main/java/com/example/foundation/sideeffects/dialogs/plugin/DialogsSideEffectMediator.kt
+++ app/src/main/java/com/example/foundation/sideeffects/dialogs/plugin/DialogsSideEffectMediator.kt
@@ -1,19 +1,22 @@
 package com.example.foundation.sideeffects.dialogs.plugin
 
-import com.example.foundation.model.ErrorResult
 import com.example.foundation.model.Emitter
+import com.example.foundation.model.ErrorResult
+import com.example.foundation.model.toEmitter
 import com.example.foundation.sideeffects.SideEffectMediator
 import com.example.foundation.sideeffects.dialogs.Dialogs
+import kotlinx.coroutines.suspendCancellableCoroutine
 
 class DialogsSideEffectMediator : SideEffectMediator<DialogsSideEffectImpl>(), Dialogs {
 
     var retainedState = RetainedState()
 
-    override suspend fun show(dialogConfig: DialogConfig): Boolean = CallbackTask.create { emitter ->
+    override suspend fun show(dialogConfig: DialogConfig): Boolean = suspendCancellableCoroutine { continuation ->
+        val emitter = continuation.toEmitter()
         if (retainedState.record != null) {
             // for now allowing only 1 active dialog at a time
             emitter.emit(ErrorResult(IllegalStateException("Can't launch more than 1 dialog at a time")))
-            return@create
+            return@suspendCancellableCoroutine
         }
 
         val wrappedEmitter = Emitter.wrap(emitter) {
@@ -32,7 +35,7 @@ class DialogsSideEffectMediator : SideEffectMediator<DialogsSideEffectImpl>(), D
         }
 
         retainedState.record = record
-    }.suspend()
+    }
 
     class DialogRecord(
         val emitter: Emitter<Boolean>,
--- app/src/main/java/com/example/simplemvvmcoroutineskotlin/App.kt
+++ app/src/main/java/com/example/simplemvvmcoroutineskotlin/App.kt
@@ -2,34 +2,18 @@ package com.example.simplemvvmcoroutineskotlin
 
 import android.app.Application
 import com.example.foundation.BaseApplication
-import com.example.foundation.model.tasks.ThreadUtils
-import com.example.foundation.model.dispatchers.MainThreadDispatcher
-import com.example.foundation.model.tasks.factories.ExecutorServiceTasksFactory
-import com.example.foundation.model.tasks.factories.HandlerThreadTasksFactory
 import com.example.simplemvvmcoroutineskotlin.model.colors.InMemoryColorsRepository
-import java.util.concurrent.Executors
 
 /**
  * Here we store instances of model layer classes.
  */
 class App : Application(), BaseApplication {
 
-    // instances of all created task factories
-    private val singleThreadExecutorTasksFactory = ExecutorServiceTasksFactory(Executors.newSingleThreadExecutor())
-    private val handlerThreadTasksFactory = HandlerThreadTasksFactory()
-    private val cachedThreadPoolExecutorTasksFactory = ExecutorServiceTasksFactory(Executors.newCachedThreadPool())
-
-    private val threadUtils = ThreadUtils.Default()
-    private val dispatcher = MainThreadDispatcher()
-
     /**
      * Place your singleton scope dependencies here
      */
     override val singletonScopeDependencies: List<Any> = listOf(
-        cachedThreadPoolExecutorTasksFactory, // task factory to be used in view-models
-        dispatcher, // dispatcher to be used in view-models
-
-        InMemoryColorsRepository(cachedThreadPoolExecutorTasksFactory, threadUtils)
+        InMemoryColorsRepository()
     )
 
 }
